9029033567ac9aefdd3df73b93f665f2163e1f03
Musobek Madrimov <66286036+musobekmadrimov@users.noreply.github.com>
2021-12-18 03:35:25 +0200

    some changes..

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <h2>This is Homepage</h2>
+      <h2>This is your Homepage</h2>
+      <h2>
+        coded by{" "}
+        <a
+          href="https://github.com/musobekmadrimov"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Musobek Madrimov
+        </a>
+      </h2>
     </div>
   );
 }
